73a3dd7d2b1389af187584bdf1c932eb679a3dbf
Gru Lewis <121012648@qq.com>
2019-08-01 13:59:55 +0800

    Update build_fdbus.sh

--- build_fdbus.sh
+++ build_fdbus.sh
@@ -1,5 +1,6 @@
 
 
 
-mkdir -p build/install && cd build
-cmake -DSYSTEM_ROOT=~/workspace/protobuf/build/install -DCMAKE_INSTALL_PREFIX=install ../cmake
+mkdir -p build/install && push . && cd build
+cmake -DSYSTEM_ROOT=$DEP_PROTOBUF_PATH -DCMAKE_INSTALL_PREFIX=install ../cmake
+pop
