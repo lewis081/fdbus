11072f97edf240caca1183625a4a46d35bf83045
Gru Lewis <121012648@qq.com>
2019-08-01 13:58:26 +0800

    Update build_protobuf.sh

--- build_protobuf.sh
+++ build_protobuf.sh
@@ -1,10 +1,25 @@
 PROTOBUF_WS=protobuf_workspace
 
-mkdir $PROTOBUF_WS && cd $PROTOBUF_WS
+#record
+pwd
+
+#work
+mkdir $PROTOBUF_WS && push . && cd $PROTOBUF_WS
 git clone https://github.com/protocolbuffers/protobuf.git
-cd protobuf && git submodule update --init --recursive
-mkdir -p build/install && cd build
+push . && cd protobuf && git submodule update --init --recursive
+mkdir -p build/install && push . && cd build
 cmake -DCMAKE_INSTALL_PREFIX=install -DBUILD_SHARED_LIBS=1 ../cmake
 make -j4 install 
 ls -l install
+pop
+pop
+pop
+
+#check
+pwd
+
+ls -l $PROTOBUF_WS/protobuf/build/install
+
+#for building fdbus
+DEP_PROTOBUF_PATH=$PROTOBUF_WS/protobuf/build/install
 
